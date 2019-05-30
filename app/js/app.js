81c031b576ffcdc5d3d3bcf84a6663349f2e77d6
Tadeas Kosek <tadeas.kosek@netvor.co>
2019-05-30 21:41:25 +0200

    finish version 1.0

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,54 @@
-console.log('Done');
+// console.log('Done');
 
 $('#searchBtn').on('click', function() {
     setTimeout(function() {
         window.location = 'results.html';
-    }, 1000)
+    }, 3000)
 });
+
+function closeModal() {
+    $('.modalContainer').css('display', 'none');
+    $('body').css('overflow', "scroll");
+}
+
+function showModal(id) {
+
+    if(id == null) {
+        $('.modalContainer').css('display', 'flex');
+        // document.getElementById('modalContainer').style.display = "flex";
+    } else {
+        let modal = $(`[data-id=${id}]`);
+
+        // console.log(modal[0]);
+        $(modal[0]).css('display', 'flex');
+    }
+
+    $('body').css('overflow', "hidden");
+}
+
+function loginUser() {
+    $('.btn-nav').addClass('hidden');
+    $('.profile').removeClass('hidden');
+
+    closeModal();
+}
+
+let price = 550
+
+$('input[type="checkbox"]').on('change', function() {
+    let value = $(this).data('to-open');
+
+    if ($(`input#${value}`).prop('checked')) {
+        price += parseInt($(this).val());
+        $(`.${value}`).show(500);
+        // console.log(price);
+
+        $('#total').text(price);
+    } else {
+        price -= parseInt($(this).val());
+        $(`.${value}`).hide(500);
+        // console.log(price);
+
+        $('#total').text(price);
+    }
+})
